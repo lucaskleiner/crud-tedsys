1fbdd628a131a9a0877ab3afec6296f06da76399
Lucas R. Pereia <57834313+lucaskleiner@users.noreply.github.com>
2023-06-28 08:08:26 -0300

    Add files via upload

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Gerenciamento de Tarefas</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container mt-4">
+        @yield('content')
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
